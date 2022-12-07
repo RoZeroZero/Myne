59c956059f9f49e3cb5c3d18c0403ffc495a288f
RoZeroZero <60973481+RoZeroZero@users.noreply.github.com>
2022-12-08 00:45:48 +0700

    Changes to be committed: 	modified:   main.py 	modified:   modules/get_versions.py

--- main.py
+++ main.py
@@ -13,13 +13,16 @@ def init():
 
     def onclick_install():
         return
+    
+    def onclick_choose_item():
+        print("test")
 
     with dpg.window(tag="main"):
         vw, vh = 800, 290
         dpg.add_text("Welcome to Myne Loader for Minecraft")
         dpg.add_button(label="Exit", callback=onclick_quit, pos=[vw-70,vh-65])
         dpg.add_button(label="Install", callback=onclick_install, pos=[vw-170,vh-65])
-        dpg.add_listbox(width=190, num_items=7)
+        dpg.add_listbox(width=190, num_items=7, callback=onclick_choose_item)
         dpg.add_text("Description", wrap=vw-210, pos=[210,40])
         dpg.add_input_text(pos=[8,vh-65], width=vw-187)
 
--- modules/get_versions.py
+++ modules/get_versions.py
@@ -1,9 +1,17 @@
 import urllib.request
-
+name = []
+uri = []
+desc = []
 def versions():
-    url = "https://raw.githubusercontent.com/RoZeroZero/Myne/main/versions.txt"
-    urllib.request.urlretrieve(url, 'versions.txt')
-    with open("versions.txt") as f:
-        for line in f:
-            print(line)
-versions()
+    if len(name)==0:
+        urllib.request.urlretrieve("https://raw.githubusercontent.com/RoZeroZero/Myne/main/versions.txt", 'versions.txt')
+        with open("versions.txt") as f:
+            for line in f:
+                print("test")
+                name.append(line.split("@")[0])
+                uri.append(line.split("@")[1])
+                desc.append(line.split("@")[2])
+    return name, uri, desc
+
+def pack():
+    return
