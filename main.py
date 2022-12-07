92dcf2ef198e26c265bdf27f7db5496539266ec9
RoZeroZero <60973481+RoZeroZero@users.noreply.github.com>
2022-12-07 23:08:11 +0700

    Changes to be committed: 	new file:   assets/myne.ico 	modified:   main.py

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import dearpygui.dearpygui as dpg
-# import dearpygui.demo as demo
 import dark_theme
 
-vw = 800
-vh = 300
-
 dpg.create_context()
+
 dark_theme.create_theme_dark()
 dark_theme.create_font()
 
+def onclick_quit():
+    quit()
 
-def button_callback():
-    exit()
+def onclick_install():
+    return
 
-with dpg.window(tag="Primary Window"):
-    dpg.add_text("Myne")
-    dpg.add_button(label="Exit", callback=button_callback, pos=[0.915*vw,0.79*vh])
+with dpg.window(tag="main"):
+    vw, vh = 800, 290
+    dpg.add_text("Welcome to Myne Loader for Minecraft")
+    dpg.add_button(label="Exit", callback=onclick_quit, pos=[vw-70,vh-65])
+    dpg.add_button(label="Install", callback=onclick_install, pos=[vw-170,vh-65])
+    dpg.add_listbox(width=190, num_items=7)
+    dpg.add_text("Description", wrap=vw-210, pos=[210,40])
+    dpg.add_input_text(pos=[8,vh-65], width=vw-187)
 
-
-dpg.create_viewport(title='Myne', width=vw, height=vh, resizable=False, decorated=True)
+dpg.create_viewport(title='Myne', width=vw, height=vh, resizable=False, large_icon="assets/myne.ico")
 dpg.setup_dearpygui()
 dpg.show_viewport()
-dpg.set_primary_window("Primary Window", True)
+dpg.set_primary_window("main", True)
 dpg.start_dearpygui()
 dpg.destroy_context()
