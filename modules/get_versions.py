97186dd5b0d0c8ab04f35d881acde09b37af2647
RoZeroZero <60973481+RoZeroZero@users.noreply.github.com>
2022-12-08 00:11:49 +0700

    modified:   modules/get_versions.py 	deleted:    versions.xml

--- modules/get_versions.py
+++ modules/get_versions.py
@@ -1,11 +1,7 @@
 import urllib.request
-from xml.dom import minidom
 
 def versions():
     url = "https://raw.githubusercontent.com/RoZeroZero/Myne/main/versions.xml"
     urllib.request.urlretrieve(url, 'versions.xml')
-    mydoc = minidom.parse('versions.xml')
-    items = mydoc.getElementsByTagName('NAME')
-    for elem in items:
-        print(elem.firstChild.data)
+
 versions()
