07fb39cfca601cc46658cc73370e877b749e5010
RoZeroZero <60973481+RoZeroZero@users.noreply.github.com>
2022-12-08 00:28:26 +0700

    On branch main Your branch is behind 'origin/main' by 1 commit, and can be fast-forwarded.   (use "git pull" to update your local branch)
    Changes to be committed:
    	modified:   .vscode/settings.json
    	modified:   modules/get_versions.py

--- modules/get_versions.py
+++ modules/get_versions.py
@@ -1,7 +1,9 @@
 import urllib.request
 
 def versions():
-    url = "https://raw.githubusercontent.com/RoZeroZero/Myne/main/versions.xml"
-    urllib.request.urlretrieve(url, 'versions.xml')
-
+    url = "https://raw.githubusercontent.com/RoZeroZero/Myne/main/versions.txt"
+    urllib.request.urlretrieve(url, 'versions.txt')
+    with open("versions.txt") as f:
+        for line in f:
+            print(line)
 versions()
