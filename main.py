234a9092c6e26ba23020ff4df90a579f7da50a93
RoZeroZero <60973481+RoZeroZero@users.noreply.github.com>
2022-12-07 20:27:59 +0700

    modified:   .vscode/settings.json 	new file:   assets/minecraft.ttf 	new file:   dark_theme.py 	modified:   main.py 	deleted:    onscreen.py

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-import onscreen
-from kivy.app import App
+import dearpygui.dearpygui as dpg
+import dark_theme
 
-class MyApp(App):
+vw = 800
+vh = 300
 
-    def build(self):
-        return onscreen.LoginScreen()
+dpg.create_context()
+dpg.bind_theme(dark_theme.create_theme_dark())
+dpg.bind_font(dark_theme.create_font())
 
+def button_callback():
+    dpg.destroy_context()
 
-if __name__ == '__main__':
-    MyApp().run()
+with dpg.window(tag="Primary Window"):
+    dpg.add_text("Hello, world")
+    dpg.add_button(label="Exit", callback=button_callback, pos=[0.925*vw,0.9*vh])
+    dpg.add_input_text(label="string", default_value="Quick brown fox")
+    dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+
+dpg.create_viewport(title='Myne', width=vw, height=vh, resizable=False, decorated=False)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.set_primary_window("Primary Window", True)
+dpg.start_dearpygui()
+dpg.destroy_context()
