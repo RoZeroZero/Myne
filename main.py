6c1893c1e239c9e65553cf5e9ae2ac0c79cbb418
RoZeroZero <60973481+RoZeroZero@users.noreply.github.com>
2022-12-07 15:31:26 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   .vscode/settings.json
    	modified:   main.py
    	new file:   onscreen.py

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-import dearpygui.dearpygui as dpg
+import onscreen
+from kivy.app import App
 
-dpg.create_context()
+class MyApp(App):
 
-with dpg.window(tag="Primary Window"):
-    dpg.add_text("Hello, world")
-    dpg.add_button(label="Save")
-    dpg.add_input_text(label="string", default_value="Quick brown fox")
-    dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+    def build(self):
+        return onscreen.LoginScreen()
 
-dpg.create_viewport(title='Custom Title', width=600, height=200)
-dpg.setup_dearpygui()
-dpg.show_viewport()
-dpg.set_primary_window("Primary Window", True)
-dpg.start_dearpygui()
-dpg.destroy_context()
+
+if __name__ == '__main__':
+    MyApp().run()
