19560952e8a71996168452fde856239d2c155255
RoZeroZero <60973481+RoZeroZero@users.noreply.github.com>
2022-12-08 02:26:14 +0700

    Changes to be committed: 	modified:   .vscode/settings.json 	modified:   main.py 	modified:   modules/get_versions.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from os import abort
 import dearpygui.dearpygui as dpg
 import modules.get_versions as get
 import modules.dark_theme as dark_theme
@@ -9,22 +10,24 @@ def init():
     dark_theme.create_font()
 
     def onclick_quit():
-        quit()
+        abort()
 
     def onclick_install():
-        return
-    
-    def onclick_choose_item():
-        print("test")
+        get.pack(get.versions()[0].index(dpg.get_value("listbox")))
+
+    def onclick_choose_item(sender, app_data, user_data):#работает только так
+        dpg.set_value(item="description", value=get.versions()[2][(get.versions()[0].index(app_data))])
+        #при вызове функции описание выбирается поиском app_data из списка name,
+        #с возвращением индекса совпадения, индекс указывает номер описания в списке description
 
     with dpg.window(tag="main"):
         vw, vh = 800, 290
         dpg.add_text("Welcome to Myne Loader for Minecraft")
         dpg.add_button(label="Exit", callback=onclick_quit, pos=[vw-70,vh-65])
         dpg.add_button(label="Install", callback=onclick_install, pos=[vw-170,vh-65])
-        dpg.add_listbox(width=190, num_items=7, callback=onclick_choose_item)
-        dpg.add_text("Description", wrap=vw-210, pos=[210,40])
-        dpg.add_input_text(pos=[8,vh-65], width=vw-187)
+        dpg.add_listbox(width=300, num_items=7, items=get.versions()[0], callback=onclick_choose_item, tag="listbox")
+        dpg.add_text(default_value=get.versions()[2][0], wrap=vw-350, pos=[320,40], tag="description")
+        dpg.add_input_text(pos=[8,vh-65], width=vw-187, tag="path")
 
     dpg.create_viewport(title='Myne', width=vw, height=vh, resizable=False, large_icon="assets/myne.ico")
     dpg.setup_dearpygui()
--- modules/get_versions.py
+++ modules/get_versions.py
@@ -1,17 +1,19 @@
 import urllib.request
+
 name = []
 uri = []
 desc = []
-def versions():
+
+def versions():#переписать
     if len(name)==0:
         urllib.request.urlretrieve("https://raw.githubusercontent.com/RoZeroZero/Myne/main/versions.txt", 'versions.txt')
         with open("versions.txt") as f:
             for line in f:
-                print("test")
                 name.append(line.split("@")[0])
                 uri.append(line.split("@")[1])
                 desc.append(line.split("@")[2])
+
     return name, uri, desc
 
-def pack():
-    return
+def pack(id):
+    urllib.request.urlretrieve("https://www.dropbox.com/s/" + uri[id] + "/" + name[id] + ".zip?dl=1", name[id] + ".zip")
